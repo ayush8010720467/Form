081768a1bcb8c95e147eae60392b87f388a05dd6
ayush8010720467 <ayushkatiyar096@gmail.com>
2019-01-30 17:43:13 +0530

    Fixed the bugs related to adding the bootstrap

---
+++ src/app/enter-credentials/enter-credentials.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enter-credentials',
+  templateUrl: './enter-credentials.component.html',
+  styleUrls: ['./enter-credentials.component.css']
+})
+
+export class EnterCredentialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+var pageTitle = "HI";
